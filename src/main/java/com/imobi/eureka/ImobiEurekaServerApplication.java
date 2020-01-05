1e08bd55b771b2f7594a64f052e6983e116731ae
suprart <rafael.unianalise@gmail.com>
2020-01-05 19:06:45 -0300

    first commit

--- src/main/java/com/imobi/eureka/ImobiEurekaServerApplication.java
+++ src/main/java/com/imobi/eureka/ImobiEurekaServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class ImobiEurekaServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		SpringApplication.run(ImobiEurekaServerApplication.class, args);
 	}
 
